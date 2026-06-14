41d1fc6e53098d6fc22edd8078e385031295a142
kazufumi316 <kazufumi316@gmail.com>
2026-06-14 11:11:05 +0900

    add gem tailwind rspec factorybot i18n

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module KaigoNoGenbaApp
+module KaigoTecho
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -18,5 +18,7 @@ module KaigoNoGenbaApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
   end
 end
