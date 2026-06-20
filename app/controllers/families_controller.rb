57738753b76c1a26cacbffada637cd8875c81cea
kazufumi316 <kazufumi316@gmail.com>
2026-06-20 21:33:14 +0900

    create care_user new.html

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -2,12 +2,4 @@ class FamiliesController < ApplicationController
 
   before_action :authenticate_user!
 
-  def new
-    @family = Family.new
-    @family_member = @family.family_members.new
-    @care_user = @family.care_users.new
-  end
-
-  def create
-
 end
