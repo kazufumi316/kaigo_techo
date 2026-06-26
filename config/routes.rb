ebe5b3c4116e87d7d832cadd9d489c9f638ae98d
kazufumi316 <kazufumi316@gmail.com>
2026-06-26 18:20:12 +0900

    edit_user_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "users/show"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
