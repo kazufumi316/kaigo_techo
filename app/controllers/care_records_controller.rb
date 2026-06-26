9153be7e4489d4f982d20589476b85319605b517
kazufumi316 <kazufumi316@gmail.com>
2026-06-26 23:18:34 +0900

    edit care_record_function

--- app/controllers/care_records_controller.rb
+++ app/controllers/care_records_controller.rb
@@ -13,6 +13,7 @@ class CareRecordsController < ApplicationController
   end
 
   def save_create_select_care_user
+    session[:care_record_attributes] ||= {}
     session[:care_record_attributes] = { care_user_id: params[:care_record][:care_user_id]}
     redirect_to health_status_care_records_path
   end
@@ -20,6 +21,7 @@ class CareRecordsController < ApplicationController
   def health_status;end
 
   def save_health_status
+    session[:care_record_attributes] ||= {}
     session[:care_record_attributes][:health_status] = params[:care_record][:health_status]
     redirect_to appetite_care_records_path
   end
@@ -27,6 +29,7 @@ class CareRecordsController < ApplicationController
   def appetite;end
 
   def save_appetite
+    session[:care_record_attributes] ||= {}
     session[:care_record_attributes][:appetite] = params[:care_record][:appetite]
     redirect_to sleep_quality_care_records_path
   end
@@ -34,6 +37,7 @@ class CareRecordsController < ApplicationController
   def sleep_quality;end
 
   def save_sleep_quality
+    session[:care_record_attributes] ||= {}
     session[:care_record_attributes][:sleep_quality] = params[:care_record][:sleep_quality]
     redirect_to memo_care_records_path
   end
@@ -44,6 +48,7 @@ class CareRecordsController < ApplicationController
     final_params = session[:care_record_attributes].merge(care_record_params.to_h)
     @care_record = current_user.care_records.build(final_params)
     if @care_record.save
+      session.delete(:care_record_attributes)
       redirect_to homes_path, notice: '介護記録をつけました'
     else
       logger.error @care_record.errors.full_messages
@@ -72,6 +77,21 @@ class CareRecordsController < ApplicationController
 
   def show
     @care_record = CareRecord.find(params[:id])
+  end
+
+  def edit
+    @care_record = CareRecord.find(params[:id])
+  end
+
+  def update
+    @care_record = CareRecord.find(params[:id])
+    if @care_record.update(care_record_params)
+      redirect_to care_records_path(care_user_id: @care_record.care_user.id),
+                  notice: "#{@care_record.created_at.strftime('%-m月%-d日 %-H時%M分')}\n介護記録を更新しました"
+    else
+      flash.now[:alert] = "介護記録の更新に\n失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
