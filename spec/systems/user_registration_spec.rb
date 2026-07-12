944e208d34f0b53cb4bdbaece521293dbf76b254
kazufumi316 <kazufumi316@gmail.com>
2026-07-12 16:04:34 +0900

    rspec user_registration

--- spec/systems/user_registration_spec.rb
+++ spec/systems/user_registration_spec.rb
@@ -1,40 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'アカウント登録', type: :system do    
+RSpec.describe 'アカウント登録', type: :system do
   describe 'アカウント登録成功' do
+    include_context 'アカウント登録'
     it "アカウント登録が成功しホーム画面に遷移すること" do
-      visit new_user_registration_path
-      fill_in '氏名', with: Faker::Name.name
-      fill_in '電話番号', with: Faker::PhoneNumber.phone_number
-      fill_in 'メールアドレス', with: Faker::Internet.email
-      fill_in 'パスワード', with: 'password'
-      fill_in 'パスワード（確認用）', with: 'password'
-      click_button "登録"
-      expect(page).to have_current_path(homes_path, wait: 5)
+      expect(page).to have_current_path(homes_path)
       expect(page).to have_content "アカウント登録が完了しました"
-    end
-  end
-
-  describe 'アカウント編集成功' do
-    it "アカウント編集が成功しホーム画面に遷移すること" do
-      visit new_user_registration_path
-      fill_in '氏名', with: Faker::Name.name
-      fill_in '電話番号', with: Faker::PhoneNumber.phone_number
-      fill_in 'メールアドレス', with: Faker::Internet.email
-      fill_in 'パスワード', with: 'password'
-      fill_in 'パスワード（確認用）', with: 'password'
-      click_button "登録"
-      expect(page).to have_current_path(homes_path, wait: 5)
-      click_on "アカウント情報"
-      click_on "編集"
-      fill_in '氏名', with: '権蔵'
-      fill_in '電話番号', with: '020000000'
-      fill_in 'メールアドレス', with: 'fake@fake.com'
-      fill_in 'パスワード', with: 'password1'
-      fill_in 'パスワード（確認用）', with: 'password1'
-      click_button "更新"
-      expect(page).to have_current_path(homes_path, wait: 5)
-      expect(page).to have_content "アカウント情報を\n更新しました"
     end
   end
 
@@ -51,17 +22,28 @@ RSpec.describe 'アカウント登録', type: :system do
       expect(page).to have_content "入力にエラーがあります"
     end
   end
+end
+
+RSpec.describe 'アカウント編集', type: :system do
+  describe 'アカウント編集成功' do
+    include_context 'アカウント登録'
+    it "アカウント編集が成功しホーム画面に遷移すること" do
+      click_on "アカウント情報"
+      click_on "編集"
+      fill_in '氏名', with: '権蔵'
+      fill_in '電話番号', with: '020000000'
+      fill_in 'メールアドレス', with: 'fake@fake.com'
+      fill_in 'パスワード', with: 'password1'
+      fill_in 'パスワード（確認用）', with: 'password1'
+      click_button "更新"
+      expect(page).to have_current_path(homes_path)
+      expect(page).to have_content "アカウント情報を\n更新しました"
+    end
+  end
 
   describe 'アカウント編集失敗' do
+    include_context 'アカウント登録'
     it "アカウント編集が失敗すること" do
-      visit new_user_registration_path
-      fill_in '氏名', with: Faker::Name.name
-      fill_in '電話番号', with: Faker::PhoneNumber.phone_number
-      fill_in 'メールアドレス', with: Faker::Internet.email
-      fill_in 'パスワード', with: 'password'
-      fill_in 'パスワード（確認用）', with: 'password'
-      click_button "登録"
-      expect(page).to have_current_path(homes_path, wait: 5)
       click_on "アカウント情報"
       click_on "編集"
       fill_in '氏名', with: ""
@@ -76,17 +58,10 @@ end
 
 RSpec.describe 'アカウント削除', type: :system do    
   describe 'アカウント削除に成功' do
+    include_context 'アカウント登録'
     it "アカウント削除が成功しトップページに遷移すること" do
-      visit new_user_registration_path
-      fill_in '氏名', with: Faker::Name.name
-      fill_in '電話番号', with: Faker::PhoneNumber.phone_number
-      fill_in 'メールアドレス', with: Faker::Internet.email
-      fill_in 'パスワード', with: 'password'
-      fill_in 'パスワード（確認用）', with: 'password'
-      click_button "登録"
-      expect(page).to have_current_path(homes_path, wait: 5)
       click_on "アカウント情報"
-      accept_confirm(wait: 5) do
+      accept_confirm do
         click_on "アカウント削除"
       end
       expect(page).to have_current_path(root_path)
