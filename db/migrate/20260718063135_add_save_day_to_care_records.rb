8b913a91b827bdfa28eae1181a9db1548aaed0e1
kazufumi316 <kazufumi316@gmail.com>
2026-07-18 15:38:45 +0900

    add care_records_column

---
+++ db/migrate/20260718063135_add_save_day_to_care_records.rb
@@ -0,0 +1,11 @@
+class AddSaveDayToCareRecords < ActiveRecord::Migration[8.1]
+  def up
+    add_column :care_records, :save_day, :datetime
+    execute "UPDATE care_records SET save_day = created_at"
+    change_column_null :care_records, :save_day, false
+  end
+
+  def down
+    remove_column :care_records, :save_day
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_18_045103) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_18_063135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_18_045103) do
     t.datetime "created_at", null: false
     t.integer "health_status", null: false
     t.string "memo"
+    t.datetime "save_day", null: false
     t.integer "sleep_quality", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
