7d78cf76bdaa91cefe0783c9a918107f50c337e7
kazufumi316 <kazufumi316@gmail.com>
2026-07-18 12:53:55 +0900

    edit system spec wareki

--- spec/systems/care_user_spec.rb
+++ spec/systems/care_user_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe '見守り家族情報', type: :system do
       visit homes_path
       click_on '見守り家族情報'
       expect(page).to have_content(@dammy_name)
-      birthday = @dammy_date.strftime('%Y年%-m月%-d日')
+      birthday = @dammy_date.strftime('%Jf')
       expect(page).to have_content(birthday)
       expect(page).to have_content("A型")
       expect(page).to have_content("病名")
@@ -53,7 +53,7 @@ RSpec.describe '見守り家族情報', type: :system do
         click_on '見守り家族情報'
         click_on @dammy_name_2
         expect(page).to have_content(@dammy_name_2)
-        birthday = @dammy_date_2.strftime('%Y年%-m月%-d日')
+        birthday = @dammy_date_2.strftime('%Jf')
         expect(page).to have_content(birthday)
         expect(page).to have_content("B型")
         expect(page).to have_content("病名")
